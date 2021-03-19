5c6c7eabc008b09e8b24ba58f9a49634e0b350a5
mrtaalebi <the.doors.are.locked@gmail.com>
2021-03-19 14:51:53 +0330

    Fix missing

--- boalo/bot.py
+++ boalo/bot.py
@@ -439,7 +439,7 @@ def modify_user(chat, message):
     if not check_admin(chat):
         return
 
-    args = message.split[1:]
+    args = message.text.split[1:]
     uid = args[0]
     user = db_query(sr(), models.User,
                     models.User.id == uid,
